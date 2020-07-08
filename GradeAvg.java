b06931ad0c2e2afebc8f67644152ab3ae44e5d60
JasonJVargas <66055620+JasonJVargas@users.noreply.github.com>
2020-07-08 03:59:32 -0500

    Update GradeAvg.java

--- GradeAvg.java
+++ GradeAvg.java
@@ -1,14 +1,17 @@
 
 public class GradeAvg {
 	 public static void main(String[] args) {
-		 double quizScore1, quizScore2, quizScore3, testScore1, testScore2;
+		 double quizScore1, quizScore2, quizScore3, testScore1, testScore2, quizScoreAvg, testScoreAvg;
+		
 		 quizScore1 = 70.4;
 		 quizScore2 = 94.8;
 		 quizScore3 = 92.2;
 		 testScore1 = 87.3;
 		 testScore2 = 99.9;
-		 double quizScoreAvg = (quizScore1 + quizScore2 + quizScore3)/3;
-		 double testScoreAvg = (testScore1 + testScore2)/2;
+		 
+		 quizScoreAvg = (quizScore1 + quizScore2 + quizScore3)/3;
+		 testScoreAvg = (testScore1 + testScore2)/2;
+		
 		 System.out.println("Your average Quiz score is " + quizScoreAvg +"%");
 		 System.out.println("Your average Test score is " + testScoreAvg +"%");
 
