3c516bcf329f2e28bee79117aa0019d42e1d8be4
JasonJVargas <66055620+JasonJVargas@users.noreply.github.com>
2020-07-08 04:19:42 -0500

    Update TimeComp.java

--- TimeComp.java
+++ TimeComp.java
@@ -6,12 +6,11 @@ public class TimeComp {
 		System.out.print("Type a number of seconds to convert to hours, minutes, and seconds format: ");
 		Scanner Input = new Scanner (System.in);
 		int numSeconds = Input.nextInt();
-		int t1 = numSeconds % 60;
-		int t2 = numSeconds / 60;
-		int t3 = t2 % 60;
-		t2 = t2/60;
+		int t1 = numSeconds /3600;
+		int t2 = (numSeconds % 3600) / 60;
+		int t3 = numSeconds % 60;
 		
-		System.out.print(t2 + " hours " + t3 + " minutes " + t1 + " seconds");
+		System.out.print(t1 + " hours " + t2 + " minutes " + t3 + " Seconds");
 		
 	}
 
