fc40f822981a9a4294466fcbe059b6a26b794fce
JasonJVargas <66055620+JasonJVargas@users.noreply.github.com>
2020-07-18 04:18:33 -0500

    Update Cipher.java

--- Lab 2/Cipher.java
+++ Lab 2/Cipher.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Cipher {
@@ -6,25 +7,59 @@ public class Cipher {
 		System.out.println("Please enter a number between 0 and 19: " );
 		
 		int num1, num2, num3, num4, num5;
- 
-		int numSelected;
-	
+ 	
 		Scanner input = new Scanner(System.in);
+		
 		
 		System.out.print("Please enter your first number: " );
 			num1 = input.nextInt();
-		System.out.print("Please enter your Second number: " );
+	if (num1 > 19) {
+		System.out.println("Please read directions and try again!");}
+	else {
+	
+		System.out.print("Please enter your second number: " );
 			num2 = input.nextInt();
-		System.out.print("Please enter your Third number: " );
+			if (num2 > 19) {
+				System.out.println("Please read directions and try again!");}
+			else {
+	
+				System.out.print("Please enter your third number: " );
 			num3 = input.nextInt();
-		System.out.print("Please enter your Fourth number: " );
+			if (num3 > 19) {
+				System.out.println("Please read directions and try again!");}
+			else {
+	
+				System.out.print("Please enter your fourth number: " );
 			num4 = input.nextInt();
-		System.out.print("Please enter your Fifth number: " );
-			num5 = input.nextInt();
-		
-			int numSum = (num1 + num2 + num3 + num4 + num5);
-		System.out.println("The sum of your five numbers is: " + numSum);
-			
-	}
+			if (num4 > 19) {
+				System.out.println("Please read directions and try again!");}
+			else {
 
+		System.out.print("Please enter your fifth number: " );
+			num5 = input.nextInt();
+			
+			if (num5 > 19) {
+				System.out.println("Please read directions and try again!");}
+			else {
+
+			
+			int numSum = (num1 + num2 + num3 + num4 + num5);
+		
+			
+				System.out.println("The sum of your five numbers is: " + numSum);
+				
+				Random rand = new Random();
+				int numRand = rand.nextInt((9)+1);
+				
+				System.out.println("Your random key is : " + numRand);
+				
+				
+	}
+				
+					}
+				}
+			}
+		}	
+	}
 }
+
