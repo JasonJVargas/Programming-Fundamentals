69db7d995b49756a51ffcd4a5248a4134a135946
Jason <66055620+Jason@users.noreply.github.com>
2020-07-24 14:30:06 -0500

    Update Box.java

--- Lab 3/src/Box.java
+++ Lab 3/src/Box.java
@@ -1,9 +1,28 @@
 
+
 public class Box {
-	int size = 0;
+	// Instance variable
+	int size=0;
 
 	public static void main(String[] args) {
+		// Create a new instance of our "Box" class
+		Box box = new Box();
+		// Assign the Box size variable
+		box.size = 5;
+		// Run our method
+		box.printBox();
+	}
+	
+	// Method that will print out a box of stars
+	public void printBox() {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				// Print in line *
+				System.out.print('*'); 
+			}
+			// Print a new line (next line of the box)
+			System.out.println(); 
+		}
 
 	}
-
 }
