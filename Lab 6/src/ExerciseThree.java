3ff1f1780cabfa32b3f446ab77e0584cc2a75285
JasonJVargas <66055620+JasonJVargas@users.noreply.github.com>
2020-08-13 14:58:54 -0500

    Update ExerciseThree.java

--- Lab 6/src/ExerciseThree.java
+++ Lab 6/src/ExerciseThree.java
@@ -5,5 +5,11 @@ public class ExerciseThree {
 		// TODO Auto-generated method stub
 
 	}
+	int bar(int N) {
+		 int result = 1;
+		 for (int i=1; i<N; i*=2)
+		 result+=2;
 
+		 return result;
+		}
 }
